9a8995591a382116cab1024411d0d5019e7cf55d
Jenna Castillo <jennacodes101@gmail.com>
2022-09-03 17:26:42 +0200

    fixed error in experience mode and created seed file and seed folder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_01_213620) do
+ActiveRecord::Schema.define(version: 2022_09_03_144144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2022_09_01_213620) do
     t.string "name"
     t.text "description"
     t.string "image"
-    t.string "type"
+    t.string "exp_type"
     t.string "subtype"
     t.string "country"
     t.string "city"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'deleting data🗑 ....'
+User.destroy_all
+Experience.destroy_all
+puts 'delete SUCCESS✅'
+
+puts 'seeding users ....'
+
+Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].each do |seed|
+  load seed
+end
+
+puts '-------------'
+puts 'seeding COMPLETE ✅✅✅'
+puts 'goodbye'
+puts '-------------'
