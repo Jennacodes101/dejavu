c3259d05b1dd26bd6442ccc7522793ec042c4158
Joao Potes <joao.cpotes@gmail.com>
2022-09-06 21:00:11 +0100

    reviews in place

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,33 @@
+class ReviewsController < ApplicationController
+  before_action :find_experience, except: [:destroy]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.experience = @experience
+    if @review.save
+      redirect_to experience_path(@experience), notice: 'Experience was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to experience_path(@review.experience)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :review)
+  end
+
+  def find_restaurant
+    @experience = experience.find(params[:experience_id])
+  end
+end
