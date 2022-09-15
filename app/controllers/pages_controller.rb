542c2b11127baa5e1f92430e836ef42a876986ec
Jenna Castillo <jennacodes101@gmail.com>
2022-09-15 09:58:11 -0500

    created a barebones ugly profile page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,29 @@ class PagesController < ApplicationController
   def feed
   end
 
+  def profile
+    # set user access
+    if current_user.id == params[:id]
+      @access = 'owner'
+    else
+      @access = 'visitor'
+      @profile_user = User.where(id: params[:id]).select(:name, :username, :city)
+    end
+
+    # retrieve array of user experiences
+    @user_experiences = Experience.where(user_id: params[:id])
+
+    # Active record an array of user my_experiences (wishlist)
+    @user_my_experiences = MyExperience.where(user_id: params[:id])
+    # Active record an array of done user my_experiences
+    @user_done_experiences = MyExperience.where(user_id: params[:id],
+                              done: true)
+
+    # authorization
+
+  end
+
+
+
+
 end
