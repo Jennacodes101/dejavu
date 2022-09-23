ab44a515afbad849fcfa65165a16bb85a356291e
JoÃao Bernardo <joaogoncalob@gmail.com>
2022-09-23 23:28:44 +0100

    map solved

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,13 @@ class PagesController < ApplicationController
 
   def feed
     @experiences=Experience.where(user_id: current_user.id)
+
+    # Start PgSearch
+    if params[:query].present?
+      @experiences = Experience.global_search(params[:query])
+    end
+    geo_map
+    # End PgSearch
   end
 
   def profile
@@ -21,7 +28,6 @@ class PagesController < ApplicationController
       @profile_user = User.find_by(id: params[:id])
       @profie_user_friends = @profile_user.friends
     end
-
     # retrieve array of user experiences
     @user_experiences = Experience.where(user_id: params[:id])
 
@@ -30,10 +36,18 @@ class PagesController < ApplicationController
     # Active record an array of done user my_experiences
     @user_done_experiences = MyExperience.where(user_id: params[:id],
                               done: true)
-
   end
 
+  private
 
-
-
+  def geo_map
+    @markers = @experiences.geocoded.map do |experience|
+      {
+        lat: experience.latitude,
+        lng: experience.longitude,
+        info_window: render_to_string(partial: "shared/info_window", locals: { experience: experience }),
+        image_url: helpers.asset_url("green_marker.png")
+      }
+    end
+  end
 end
