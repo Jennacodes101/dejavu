fa4db323abb697ae93339b735e5f2b6e7494ecac
JoÃao Bernardo <joaogoncalob@gmail.com>
2022-09-24 11:05:12 +0100

    seeds almost saved

--- db/seeds/2exp_seed.rb
+++ db/seeds/2exp_seed.rb
@@ -15,12 +15,18 @@ name_seed = [
 
 description_seed = [
   'Best tacos in Madrid. In Mercado de San Fernando, You have to order. The Al Pastor and Cochinita Pibil!
-  Also a Michalada!!',"Cheap tailor in the middle of La Latina. Ask for Juan, he
-  hems pants for only 5€", "The best market in madrid. Every sunday from 8-2pm.
+  Also a Michalada!!',
+  "Cheap tailor in the middle of La Latina. Ask for Juan, he
+  hems pants for only 5€",
+  "The best market in madrid. Every sunday from 8-2pm.
   It spans the whole neighborhood. Great place to buy vintage stuff and household
-  goods!",'Crazy good brunch spot, they do not take reservations so be prepared
-  to wait', 'expensive but worth it!','find a city bike and go! (watch for cars)',
-  'the best board game cafe in Lisboa. Snacks are ok','good place to chill',
+  goods!",
+  'Crazy good brunch spot, they do not take reservations so be prepared
+  to wait',
+  'expensive but worth it!',
+  'find a city bike and go! (watch for cars)',
+  'the best board game cafe in Lisboa. Snacks are ok',
+  'good place to chill',
   'Great afternoon sailing! We went for the 2pm tour and even though the air temp was 36 degrees it was cool down on
   the water. Nigel and David were a great laugh and made sure the drink and food was flowing � …'
 ]
@@ -29,18 +35,20 @@ type_seed = ['food','service','market','food','museum', 'activity',
   'cafe', 'park','excursion']
 
 address_seed = [
-  'C. de Embajadores, 41, local 36, 28012 Madrid',
+  'Calle De Embajadores, 28045 Madrid, provincia de Madrid, Espanha',
   'C. de Calatrava, 7, 28005 Madrid',
-  '40.41086295424183, -3.7072753793356163','R. da Palmeira 23, 1200-311 Lisboa, Portugal',
-  'Esplanada Dom Carlos I s/nº, 1990-005 Lisboa, Portugal',
-  'Belem, lisboa, portugal','Rua Professor Fernando da Fonseca 19, 1600-660 Lisboa, Portugal',
+  '40.41086295424183, -3.7072753793356163',
+  'R. da Palmeira 23, 1200-311 Lisboa, Portugal',
+  'Esplanada Dom Carlos I, 1990-170 Lisboa, Distrito de Lisboa, Portugal',
+  'Belem, lisboa, portugal',
+  'Rua Professor Fernando Da Fonseca, 1600-618 Lisboa, Distrito de Lisboa, Portugal',
   'Parque Eduardo VII, 1070-051 Lisboa, Portugal',
   'Doca de Santo Amaro, 1350-353 Lisboa, Portugal'
 ]
 
 price_seed = [7,5.00, 0,15.50,20, 0, 10,0, 140.50]
 
-userid_seed = [1, 1, 1, 2, 2, 2, 3, 3,3 ]
+userid_seed = [1, 1, 1, 2, 2, 2, 3, 3, 3 ]
 
 contact_seed = [
   '686633229','603666211','', '+351213421232',
@@ -52,8 +60,10 @@ url_seed= [
   'https://www.instagram.com/juantiendacostura/',
   'https://www.introducingmadrid.com/el-rastro',
   'https://www.instagram.com/seagullmethodcafe/',
-  'http://www.oceanario.pt/','http://pow-wow.pt/',
-  'http://www.cm-lisboa.pt/','http://www.sailingwithnigel.com/'
+  'http://www.oceanario.pt/',
+  'http://pow-wow.pt/',
+  'http://www.cm-lisboa.pt/',
+  'http://www.sailingwithnigel.com/'
 
 
   ]
