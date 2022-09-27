9fc5737bbf45a8a60e462ca2072854df9c91d204
JoÃao Bernardo <joaogoncalob@gmail.com>
2022-09-27 11:40:33 +0100

    seeds updated with miguel

--- db/seeds/1user_seed.rb
+++ db/seeds/1user_seed.rb
@@ -11,12 +11,20 @@ User.create(email:'joaop@test.com', password:'dejavulogin',
   driven by creative and visual thinking 🧠',phone_number: '+351123345678' )
 
 User.create(email:'joaob@test.com', password:'dejavulogin',
-  username:'divinebolt', birthday:'01/01/1993', first_name:'João',
+  username:'divinebolt', birthday:'18/11/1979', first_name:'João',
   last_name:'Gonçalo',bio:'I have always been passionated for tech, computers,
   board games, and sushi🍣', city:'Lisboa, Portugal',
   phone_number:'+351123345654')
 
-  if User.count != 3
+User.create(email:'miguel@test.com', password:'dejavulogin',
+  username:'mifig', birthday:'01/01/1993', first_name:'Miguel',
+  last_name:'Figueiredo',bio:'I am a recent graduate in Finance,
+  eager to learn programming languages to be able to materialize
+  my own ideas by creating products or services without the need
+  to outsource those skills.', city:'Lisboa, Portugal',
+  phone_number:'+35962345654')
+
+  if User.count != 4
     puts "USER COUNT ERROR, USER COUNT IS #{User.count}"
     exit(false)
   end
--- db/seeds/2exp_seed.rb
+++ db/seeds/2exp_seed.rb
@@ -7,10 +7,12 @@ name_seed = [
   'Seagull Method Cafe',
   'Oceanário de Lisboa',
   'Biking in Belem',
-  'board games @ Pow Wow',
+  'Gárgula Gaming',
   'Parque Eduardo VII',
-  'Sailing with Nigel'
-
+  'Sailing with Nigel',
+  'Parte Chão by Galgo',
+  'Le wagon - Lisboa',
+  'Le Villa'
 ]
 
 description_seed = [
@@ -25,57 +27,70 @@ description_seed = [
   to wait',
   'expensive but worth it!',
   'find a city bike and go! (watch for cars)',
-  'the best board game cafe in Lisboa. Snacks are ok',
+  'Perfect place to visit if you love all kinds of games and want to have a cozy place to play them. The food is not bad, either!',
   'good place to chill',
   'Great afternoon sailing! We went for the 2pm tour and even though the air temp was 36 degrees it was cool down on
-  the water. Nigel and David were a great laugh and made sure the drink and food was flowing � …'
+  the water. Nigel and David were a great laugh and made sure the drink and food was flowing � …',
+  'Phenomenal band of music',
+  'Learn Software Engineering and Data Science on campus or online - Join a community of career changers,
+  tech creators, entrepreneurs who changed their lives with us.',
+  'Its a perfect place to spend quality time with friends and family.
+  It got such a relaxed and calm atmosphere, which is due to both the design of
+  the restaurant and the park where its located'
 ]
 
 type_seed = ['food','service','market','food','museum', 'activity',
-  'cafe', 'park','excursion']
+  'cafe', 'park','excursion', 'nighlife', 'classes', 'food']
 
 address_seed = [
-  'Calle De Embajadores, 28045 Madrid, provincia de Madrid, Espanha',
+  'Calle De Embajadores, 28045 Madrid, Espanha',
   'C. de Calatrava, 7, 28005 Madrid',
   '40.41086295424183, -3.7072753793356163',
   'R. da Palmeira 23, 1200-311 Lisboa, Portugal',
-  'Esplanada Dom Carlos I, 1990-170 Lisboa, Distrito de Lisboa, Portugal',
+  'Esplanada Dom Carlos I, 1990-170 Lisboa, Portugal',
   'Belem, lisboa, portugal',
-  'Rua Professor Fernando Da Fonseca, 1600-618 Lisboa, Distrito de Lisboa, Portugal',
+  'Rua José Galhardo, 1750-131 Lisboa, Distrito de Lisboa, Portugal',
   'Parque Eduardo VII, 1070-051 Lisboa, Portugal',
-  'Doca de Santo Amaro, 1350-353 Lisboa, Portugal'
+  'Doca de Santo Amaro, 1350-353 Lisboa, Portugal',
+  'Largo Do Intendente Pina Manique, 1100-285 Lisboa, Distrito de Lisboa, Portugal',
+  'Rua Do Conde De Redondo, 1150-102 Lisboa, Distrito de Lisboa, Portugal',
+  'Campo Dos Mártires Da Pátria, 1150-225 Lisboa, Distrito de Lisboa, Portugal'
 ]
 
-price_seed = [7,5.00, 0,15.50,20, 0, 10,0, 140.50]
+price_seed = [7,5.00, 0,15.50,20, 0, 10,0, 140.50, 15, 6000, 20]
 
-userid_seed = [1, 1, 1, 2, 2, 2, 3, 3, 3 ]
+userid_seed = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4 ]
 
 contact_seed = [
   '686633229','603666211','', '+351213421232',
-  '+351218917000','','+351217524315', '','+351912024749'
+  '+351218917000','','+351217524315', '','+351912024749', '' , '', ''
   ]
 
 url_seed= [
+  'https://taquerialatentacion.com/',
   'https://mercadodesanfernando.es/',
   'https://www.instagram.com/juantiendacostura/',
   'https://www.introducingmadrid.com/el-rastro',
   'https://www.instagram.com/seagullmethodcafe/',
   'http://www.oceanario.pt/',
-  'http://pow-wow.pt/',
+  'https://gargulagaming.pt/',
   'http://www.cm-lisboa.pt/',
-  'http://www.sailingwithnigel.com/'
-
-
+  'http://www.sailingwithnigel.com/',
+  'https://galgogalgogalgo.bandcamp.com/album/parte-ch-o',
+  'https://www.lewagon.com/lisbon',
+  'http://www.cm-lisboa.pt/'
   ]
 
   city_seed = [
     'Madrid', 'Madrid', 'Madrid',
+    'Lisboa', 'Lisboa', 'Lisboa',
     'Lisboa', 'Lisboa', 'Lisboa',
     'Lisboa', 'Lisboa', 'Lisboa'
   ]
 
   country_seed = [
     'Spain', 'Spain', 'Spain',
+    'Portugal', 'Portugal', 'Portugal',
     'Portugal', 'Portugal', 'Portugal',
     'Portugal', 'Portugal', 'Portugal'
   ]
